c8447fcff7e89a7c31e76d30fa0cbf9ceddea183
Dan <507360715@qq.com>
2023-12-18 18:20:07 +0800

    add linux support

--- DST_ENTRY_POINT.py
+++ DST_ENTRY_POINT.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -6,7 +8,10 @@ from const import *
 file_list = ["Distributed_con_node_i.py"]*DST_NODE_NUM + ["Distributed_acc_node_i.py"]*DST_ACC_NUM
 
 def run_and_sleep(file):
-    os.system(f"start python {file}")
+    if os.name == 'nt':
+        os.system(f"start /B python {file}")
+    else:
+        os.system(f"python {file} &")
     time.sleep(1)
 
 with ThreadPoolExecutor() as executor:
